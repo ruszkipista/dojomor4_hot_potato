39ce1719d9cf5ee102e99ad56c6e3ebd9f892139
ruszkipista <ruszkipista@users.noreply.github.com>
2020-10-24 11:04:38 +0100

    blink 2 times in 1 second

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+let timer = 0
+function doBlink (timer: number) {
+    led.toggle(2, timer % 5)
+}
+input.onButtonPressed(Button.AB, function () {
+    timer = randint(5, 20)
+    basic.showIcon(IconNames.Ghost)
+    while (timer >= 0) {
+        doBlink(timer)
+        basic.pause(500)
+        doBlink(timer)
+        basic.pause(500)
+        timer += -1
+    }
+    basic.showIcon(IconNames.No)
 })
